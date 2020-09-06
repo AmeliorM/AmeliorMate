7e060b94a33b928ea188cbcf3598ebd22524c727
worldwidekatie <katie.evankodouglas@gmail.com>
2020-09-06 15:12:21 -0600

    removed parameters

--- GPT3.py
+++ GPT3.py
@@ -9,7 +9,6 @@ class ChatBot():
     instructions="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.",
     examples=[("Hello, who are you?", "I am an AI created by OpenAI. How can I help you today?")],
     engine="davinci",
-    prompt=None,
     max_tokens=150,
     temperature=.9,
     top_p=1,
@@ -19,27 +18,28 @@ class ChatBot():
 
         self.bot = bot + ": "
         self.user = user + ": "
+        self.examples = examples
+        self.instructions = instructions
         self.engine = engine
         self.presence_penalty = presence_penalty,
         self.frequency_penalty = frequency_penalty
-        self.prompt = prompt
         self.max_tokens = max_tokens
         self.temperature = temperature
         self.top_p = top_p
         self.stop = stop
+
         if self.stop == True:
             self.stop = [self.user, self.bot]
-            self.examples = examples
-            self.instructions = instructions
-        if self.prompt == None:
-            self.prompt = self.instructions + "\r\n\r\n"
-            self. intro = self.instructions + "\r\n"
+
+        self.prompt = self.instructions + "\r\n\r\n"
+        self.intro = self.instructions + "\r\n"
         
         if self.examples != None:
             self.intro = self.prompt + self.user + self.examples[0][0] + "\r\n" + self.bot + self.examples[0][1] + "\r\n\r\n"
             for i in self.examples:
                 self.prompt += self.user + i[0] + "\r\n" + self.bot + i[1] + "\r\n\r\n"
-    
+        
+
     def set_key(self, key):
         """A method to set your OpenAI API key"""
         openai.api_key = key
