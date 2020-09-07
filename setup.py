91db899252704a1b60c80b7b89244bcf030e3d95
worldwidekatie <katie.evankodouglas@gmail.com>
2020-09-07 16:33:57 -0600

    updated readme

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-REQUIRED = [
-    "openai"
-]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="AmeliorMate-AmeliorM",
-    version="0.0.2",
+    version="0.0.4",
     author="Katie Evanko-Douglas",
     author_email="katie@ameliormate.com",
     description="A a few methods for making GPT3 chatbots",
@@ -17,7 +14,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/AmeliorM/AmeliorMate",
     packages=setuptools.find_packages(),
-    install_requires=REQUIRED,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
